45873725f0cb7c7f1a8f0a26aad17b59f572c766
gittin <fiftyaa@yahoo.com>
2015-11-19 23:37:22 +0100

    added a folder

---
+++ afo/README.md
@@ -0,0 +1,5 @@
+# testingGit
+This is the description space.
+
+I made this change on my puter...
+after cloning it from github using 'git clone'
---
+++ afo/helloGit.java
@@ -0,0 +1,10 @@
+
+public class helloGit
+{
+ public static void main(final String... args)
+ throws Exception
+ {
+  for(int u=0;u<forever;u++)
+  System.out.println(" hello git.... ");
+ }
+}
